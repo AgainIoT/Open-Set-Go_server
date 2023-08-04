5483d6ecee3b71acd0ae24711f43be3e834c2ce5
bentshrimp <bent_shrimp@kookmin.ac.kr>
2023-08-04 17:51:12 +0900

    Update : exception handling & remove dummy data
    Every function prints its result.
    Removed unnecessary data
    
    Signed-off-by: bentshrimp <bent_shrimp@kookmin.ac.kr>

--- OctoClient.js
+++ OctoClient.js
@@ -36,7 +36,7 @@ export class OctoClient {
         name: repoName,
         auto_init: readme,
       });
-
+      console.log(response);
       console.log(`Repository '${repoName}' has been created successfully.`);
     } catch (error) {
       console.error(`Failed to create repository. Error: ${error.message}`);
@@ -57,7 +57,9 @@ export class OctoClient {
         });
         console.log(response);
       }
-      console.log(`Added files Repository ${repoName}`);
+      console.log(
+        `Files has been added successfully to the repository ${repoName}`,
+      );
     } catch (error) {
       console.error(
         `Failed to add files to the repository. Error: ${error.message}`,
@@ -66,50 +68,66 @@ export class OctoClient {
   };
 
   // set admin branch protection of the repository
-  setAdminBranchProtection = async (repo, branch) => {
-    this.octokit.repos.setAdminBranchProtection({
-      owner: this.username,
-      repo: repo,
-      branch: branch,
-    });
+  setAdminBranchProtection = async (repoName, branch) => {
+    try {
+      const response = await this.octokit.repos.setAdminBranchProtection({
+        owner: this.username,
+        repo: repoName,
+        branch: branch,
+      });
+      console.log(response);
+      console.log(
+        `Admin branch protection for ${repoName} has been set successfully.`,
+      );
+    } catch (error) {
+      console.log(
+        `Faild to set admin branch protection. Error: ${error.message}`,
+      );
+    }
   };
 
   // update branch protection of the repository
-  updateBranchProtection = async () => {
-    this.octokit.repos.updateBranchProtection({
-      owner: this.username,
-      repo: "test",
-      branch: "main",
-      required_status_checks: {
-        strict: true,
-        checks: [{ context: "success" }],
-      },
-      enforce_admins: true,
-      required_pull_request_reviews: {
-        dismissal_restrictions: {
-          users: ["bentshrimp"],
+  updateBranchProtection = async (repoName, branch, users) => {
+    try {
+      const response = await this.octokit.repos.updateBranchProtection({
+        owner: this.username,
+        repo: repoName,
+        branch: branch,
+        required_status_checks: {
+          strict: true,
+          checks: [{ context: "success" }],
+        },
+        enforce_admins: true,
+        required_pull_request_reviews: {
+          dismissal_restrictions: {
+            users: users,
+            teams: [""],
+          },
+          dismiss_stale_reviews: true,
+          require_code_owner_reviews: true,
+          required_approving_review_count: 2,
+          require_last_push_approval: true,
+          bypass_pull_request_allowances: {
+            users: users,
+            teams: [""],
+          },
+        },
+        restrictions: {
+          users: users,
           teams: [""],
         },
-        dismiss_stale_reviews: true,
-        require_code_owner_reviews: true,
-        required_approving_review_count: 2,
-        require_last_push_approval: true,
-        bypass_pull_request_allowances: {
-          users: ["bentshrimp"],
-          teams: [""],
-        },
-      },
-      restrictions: {
-        users: ["bentshrimp"],
-        teams: [""],
-      },
-      required_linear_history: true,
-      allow_force_pushes: true,
-      allow_deletions: true,
-      block_creations: true,
-      required_conversation_resolution: true,
-      lock_branch: true,
-      allow_fork_syncing: true,
-    });
+        required_linear_history: true,
+        allow_force_pushes: true,
+        allow_deletions: true,
+        block_creations: true,
+        required_conversation_resolution: true,
+        lock_branch: true,
+        allow_fork_syncing: true,
+      });
+      console.log(response);
+      console.log(`Branch protection for ${branch} has been updated successfully.`);
+    } catch (error) {
+      console.log(`Faild to update branch protection. Error: ${error.message}`);
+    }
   };
 }
