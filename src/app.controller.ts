1f21e6fa41498a5d1a447ae31dd253e59b8a0227
bentshrimp <bent_shrimp@kookmin.ac.kr>
2023-08-12 22:17:16 +0900

    Feat : Add api creating a github repository #11 #14
    - Validate repository name and create a github repository.
    - Validaing function is not working yet.
    - Repository creating function works well.
    
    Signed-off-by: bentshrimp <bent_shrimp@kookmin.ac.kr>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Response } from 'express';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/')
+  createRepo(
+    @Body('token') token: string,
+    @Body('userName') userName: string,
+    @Body('repoName') repoName: string,
+    @Res() res: Response,
+  ) {
+    this.appService.createRepo(token, userName, repoName);
+    res.status(200).send('ok');
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { Octokit } from '@octokit/rest';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  // Check whether this repository name does exist already.
+  checkRepo = async (
+    octokit: Octokit,
+    userName: string,
+    repoName: string,
+    // Still working on how to use this promise return.
+  ): Promise<boolean> => {
+    const response = await octokit.repos.get({
+      owner: userName,
+      repo: repoName,
+    });
+    if (Math.floor(response.status / 100) === 2) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // Create a repository.
+  createRepo = async (
+    GHtoken: string,
+    userName: string,
+    repoName: string,
+  ): Promise<void> => {
+    const octokit = new Octokit({ auth: GHtoken });
+    try {
+      // Validate the repository name.
+      const validate = await Promise.resolve(
+        this.checkRepo(octokit, userName, repoName),
+      );
+      console.log(validate);
+      if (validate) {
+        const response = await octokit.repos.createForAuthenticatedUser({
+          name: repoName,
+          auto_init: true,
+        });
+        console.log(response.headers);
+        console.log(`Repository '${repoName}' has been created successfully.`);
+      }
+    } catch (error) {
+      console.error(`Failed to create repository. Error: ${error.message}`);
+    }
+  };
 }
