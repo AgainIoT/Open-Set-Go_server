c98c1ee2094c3129ed215c12bba7638557120621
bentshrimp <bent_shrimp@kookmin.ac.kr>
2023-08-16 19:42:20 +0900

    Fix : Fixed wrong conflict solving #18
    - checkRepo() function had was fixed in wrong way, but now it's perfect
    
    Signed-off-by: bentshrimp <bent_shrimp@kookmin.ac.kr>

--- src/repo/repo.service.ts
+++ src/repo/repo.service.ts
@@ -3,21 +3,27 @@ import { Octokit } from '@octokit/rest';
 
 @Injectable()
 export class RepoService {
-  // Check whether this repository name does exist already.
   checkRepo = async (
     octokit: Octokit,
     userName: string,
     repoName: string,
-    // Still working on how to use this promise return.
   ): Promise<boolean> => {
-    const response = await octokit.repos.get({
-      owner: userName,
-      repo: repoName,
-    });
-    if (Math.floor(response.status / 100) === 2) {
-      return true;
-    } else {
-      return false;
+    try {
+      const response = await octokit.repos.get({
+        owner: userName,
+        repo: repoName,
+      });
+      // This repository already exists.
+      if (response.status === 200) {
+        throw new Error('This repository name already exists');
+      }
+    } catch (error) {
+      // You can make a repository of this name.
+      if (error.status === 404) {
+        return true;
+      }
+      // Something got wrong.
+      throw error;
     }
   };
 
@@ -29,18 +35,16 @@ export class RepoService {
   ): Promise<void> => {
     const octokit = new Octokit({ auth: GHtoken });
     try {
+      const validate = await this.checkRepo(octokit, userName, repoName);
       // Validate the repository name.
-      const validate = await Promise.resolve(
-        this.checkRepo(octokit, userName, repoName),
-      );
-      console.log(validate);
       if (validate) {
         const response = await octokit.repos.createForAuthenticatedUser({
           name: repoName,
           auto_init: true,
         });
-        console.log(response.headers);
-        console.log(`Repository '${repoName}' has been created successfully.`);
+        console.log(
+          `[${response.status}] Repository '${repoName}' has been created successfully.`,
+        );
       }
     } catch (error) {
       console.error(`Failed to create repository. Error: ${error.message}`);
