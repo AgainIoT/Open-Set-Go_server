1030197183bd9d5c1b8703dd5a43df857a442582
bentshrimp <bent_shrimp@kookmin.ac.kr>
2023-08-18 01:29:37 +0900

    Feat : add uploadFiles() function #19
    - Implemented a function that upload or update multiple files at one commit.
    
    Signed-off-by: bentshrimp <bent_shrimp@kookmin.ac.kr>

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -6,7 +6,18 @@ export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('')
-  getFiles(@Body('gitignore') gitignore: string, @Res() res: Response) {
-    res.status(200).send(gitignore);
+  uploadFiles(
+    @Body('token') token: string,
+    @Body('userName') userName: string,
+    @Body('repoName') repoName: string,
+    @Res() res: Response,
+  ) {
+    this.filesService.uploadFiles(token, userName, repoName, [
+      { path: '1.txt', content: 'example1' },
+      { path: '2.txt', content: 'example2' },
+      { path: '3.txt', content: 'example3' },
+      { path: '4.txt', content: 'example4' },
+    ]);
+    res.status(200).send('ok');
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,7 +1,59 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { Octokit } from '@octokit/rest';
 
 @Injectable()
 export class FilesService {
   constructor(private readonly httpService: HttpService) {}
+  uploadFiles = async (
+    GHtoken: string,
+    userName: string,
+    repoName: string,
+    files: { path: string; content: string }[],
+  ) => {
+    const octokit = new Octokit({ auth: GHtoken });
+    try {
+      // get the branch data including current commit hash
+      const { data: branchData } = await octokit.rest.repos.getBranch({
+        owner: userName,
+        repo: repoName,
+        branch: 'main',
+      });
+
+      const baseTree = branchData.commit.commit.tree.sha;
+
+      // add files into the tree
+      const tree = await octokit.rest.git.createTree({
+        owner: userName,
+        repo: repoName,
+        base_tree: baseTree,
+        tree: files.map((file) => ({
+          path: file.path,
+          content: file.content,
+          mode: '100644',
+        })),
+      });
+
+      // create a new commit
+      const newCommit = await octokit.rest.git.createCommit({
+        owner: userName,
+        repo: repoName,
+        message: 'Initial commit',
+        tree: tree.data.sha,
+        parents: [branchData.commit.sha],
+      });
+
+      // update the branch
+      await octokit.rest.git.updateRef({
+        owner: userName,
+        repo: repoName,
+        ref: 'heads/main',
+        sha: newCommit.data.sha,
+      });
+
+      console.log('Files uploaded and committed successfully.');
+    } catch (error) {
+      console.error('Error uploading files:', error);
+    }
+  };
 }
