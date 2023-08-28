1598c81f5fee264f32da951415ff36cb28707991
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2023-08-28 18:03:50 +0900

    Feat : Ability to get license information #43

--- src/file/license/license.controller.ts
+++ src/file/license/license.controller.ts
@@ -1,10 +1,20 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { LicenseService } from './license.service';
 
 @Controller('file/license')
 export class LicenseController {
+  constructor(private readonly licenseService: LicenseService) {}
   @Get()
   makeLicense(@Res() res: Response) {
-    res.status(200).send('ok');
+    this.licenseService
+      .getLicense()
+      .then((licenselist) => {
+        console.log(licenselist);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    res.sendStatus(200);
   }
 }
--- src/file/license/license.service.ts
+++ src/file/license/license.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { join } from 'path';
+import { load } from 'js-yaml';
+import { readFileSync } from 'fs';
+import * as fs from 'fs/promises';
 
 @Injectable()
-export class LicenseService {}
+export class LicenseService {
+  async readLicenseFile(filePath: string): Promise<any> {
+    try {
+      const fileContent = await readFileSync(filePath, 'utf-8');
+      return load(fileContent);
+    } catch (error) {
+      console.error('Error reading license file:', error);
+      return null;
+    }
+  }
+
+  async getLicense(): Promise<string> {
+    try {
+      const licensePath = join(
+        __dirname,
+        '..',
+        '..',
+        '..',
+        'src',
+        'file',
+        'license',
+        'licenseTemplate',
+      );
+
+      const files = await fs.readdir(licensePath);
+      const licenseList = [];
+
+      for (const file of files) {
+        const LicenseData = await this.readLicenseFile(join(licensePath, file));
+        licenseList.push(LicenseData);
+      }
+
+      return JSON.stringify(licenseList);
+    } catch (error) {
+      console.error('Error fetching license:', error);
+      throw error;
+    }
+  }
+}
