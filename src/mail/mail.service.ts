9bfa667bccb99102e5a8dd6dcce737f142505405
Kim-Jiyun <84488029+Kim-Jiyun@users.noreply.github.com>
2023-09-04 07:05:49 +0900

    Feat : Make the contents of the mail into html #78

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,27 +1,39 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable, Logger } from '@nestjs/common';
+import { join } from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
-
   // send mail with nodemailer
   async sendMail(userName: string, userMail: string): Promise<number> {
-    await this.mailerService
-      .sendMail({
-        to: `${userName} <${userMail}>`,
-        subject:
-          'Thank you for generating an open source project with Open-Set-Go!',
-        html: `<h2>Guide for using Open-Set-Go template<h2>
-                <p>testtest</p>`,
-      })
-      .then((result) => {
-        Logger.log(result);
-      })
-      .catch((error) => {
-        Logger.log(error);
-        return 500;
-      });
-    return 200;
+    try {
+      console.log(__dirname);
+      const filePath = join(__dirname, '..', '..', 'src', 'mail', 'mail.html');
+      console.log(filePath);
+
+      // 파일을 동기적으로 읽기
+      const htmlContent = fs.readFileSync(filePath, 'utf8');
+      console.log(htmlContent);
+
+      await this.mailerService
+        .sendMail({
+          to: `${userName} <${userMail}>`,
+          subject:
+            'Thank you for generating an open source project with Open-Set-Go!',
+          html: htmlContent,
+        })
+        .then((result) => {
+          Logger.log(result);
+        })
+        .catch((error) => {
+          Logger.log(error);
+          return 500;
+        });
+      return 200;
+    } catch (error) {
+      console.error('Error fetching HTML file:', error);
+    }
   }
 }
