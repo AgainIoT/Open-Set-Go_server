c8f1285a57066945d3f9af821f1f0cfcdff96241
ymw0407 <yunminwo1211@gmail.com>
2023-10-11 23:56:06 +0900

    Feat : review/file controller added & pr, issue, contributing, readme api added #140

--- src/review/file/file.controller.ts
+++ src/review/file/file.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { AuthService } from 'src/auth/auth.service';
-import { FileService } from './file.service';
+import { FileService, issueFile } from './file.service';
 import JwtAuthenticationGuard from 'src/auth/jwt/jwt-authentication.guard';
 import { Request, Response } from 'express';
 
@@ -18,6 +18,7 @@ export class FileController {
   async pr(
     @Body('owner') owner: string,
     @Body('repoName') repoName: string,
+    @Body('content') content: string,
     @Req() req: Request,
     @Res() res: Response,
   ) {
@@ -26,6 +27,81 @@ export class FileController {
     );
     const user = await this.userService.getUserById(jwtAccessToken);
 
-    return res.send();
+    const pr = await this.fileService.pr(
+      user.accessToken,
+      owner,
+      repoName,
+      content,
+    );
+    return res.send(pr);
+  }
+
+  @Post('issue')
+  @UseGuards(JwtAuthenticationGuard)
+  async issue(
+    @Body('owner') owner: string,
+    @Body('repoName') repoName: string,
+    @Body('issues') issues: issueFile[],
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const jwtAccessToken = this.authService.decodeToken(
+      req.cookies.Authentication,
+    );
+    const user = await this.userService.getUserById(jwtAccessToken);
+
+    const issue = await this.fileService.issue(
+      user.accessToken,
+      owner,
+      repoName,
+      issues,
+    );
+    return res.send(issue);
+  }
+
+  @Post('contributing')
+  @UseGuards(JwtAuthenticationGuard)
+  async contributing(
+    @Body('owner') owner: string,
+    @Body('repoName') repoName: string,
+    @Body('content') content: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const jwtAccessToken = this.authService.decodeToken(
+      req.cookies.Authentication,
+    );
+    const user = await this.userService.getUserById(jwtAccessToken);
+
+    const contributing = await this.fileService.contributing(
+      user.accessToken,
+      owner,
+      repoName,
+      content,
+    );
+    return res.send(contributing);
+  }
+
+  @Post('readme')
+  @UseGuards(JwtAuthenticationGuard)
+  async readme(
+    @Body('owner') owner: string,
+    @Body('repoName') repoName: string,
+    @Body('content') content: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const jwtAccessToken = this.authService.decodeToken(
+      req.cookies.Authentication,
+    );
+    const user = await this.userService.getUserById(jwtAccessToken);
+
+    const readme = await this.fileService.readme(
+      user.accessToken,
+      owner,
+      repoName,
+      content,
+    );
+    return res.send(readme);
   }
 }
--- src/review/file/file.module.ts
+++ src/review/file/file.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [AuthModule, UserModule, FileModule],
   controllers: [FileController],
   providers: [FileService],
 })
